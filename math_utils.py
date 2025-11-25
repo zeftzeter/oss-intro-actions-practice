b0ac82b5d1e1e78ce9ce5a6706c3a78364bc7da1
gurami85 <gurami17@naver.com>
2025-11-25 09:26:06 +0900

    Add files via upload

---
+++ math_utils.py
@@ -0,0 +1,12 @@
+# math_utils.py
+
+def add(a: int, b: int) -> int:
+    """두 수의 합을 반환합니다."""
+    # TODO: return a + b 로 구현하세요
+    pass
+
+
+def is_even(n: int) -> bool:
+    """짝수면 True, 홀수면 False"""
+    # TODO: 짝수 판별 코드를 작성하세요
+    pass
---
+++ test_math_utils.py
@@ -0,0 +1,12 @@
+# test_math_utils.py
+from math_utils import add, is_even
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_is_even():
+    assert is_even(2) is True
+    assert is_even(3) is False
+    assert is_even(0) is True
